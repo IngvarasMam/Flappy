e675743370ca6b0fff6fce580e833a19ad6f91a8
IngvarasMam <ingvarastwitch@gmail.com>
2023-03-18 13:24:43 +0200

    Jump Sound, Pipe, UI

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public AudioSource audio;
+    public AudioSource touched;
     private void Update()
     {
+        if (Input.anyKey) audio.Play();
         if (Input.anyKey) Jump();
+        
     }
     void Jump()
     {
         rb.velocity = new Vector2(0, 5);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            touched.Play();
+        }
     }
 }
